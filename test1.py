f9013353c6d2fc037c141e6ae2eb9aa7fff3be91
Pushpendra <132941962+PS8966@users.noreply.github.com>
2026-06-16 17:28:35 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -8,3 +8,22 @@ with test_file.open('a', encoding='utf-8') as file:
 with test_file.open('r', encoding='utf-8') as file:
     file_content = file.read()
 print(file_content)
+
+try:   
+#lines to write
+  lines_to_write = [
+      "user,password,id",
+      "john,223@,100",
+      "adam,243@,101"
+      ]
+
+  with test_file.open('w', encoding='utf-8') as f:
+      f.writelines(f"{line}\n" for line in lines_to_write )
+except Exception as e:
+    print(f" error - {e}")
+
+# Read and write
+with test_file.open('r+', encoding='utf-8') as file:
+    file.write("Added content\n")
+    file_content = file.read()
+print(file_content)
